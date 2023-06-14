adf9ccf15779814195abb2460ac2d84a1b7a2aad
Sema Yasharova <semayasharova@gmail.com>
2023-06-14 23:53:45 +0300

    New Exercise

---
+++ C#/Exam Preparation/Bonus Scoring System/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberOfStudents = int.Parse(Console.ReadLine());
+        int numberOfLectures = int.Parse(Console.ReadLine());
+        int additionalBonus = int.Parse(Console.ReadLine());
+
+        string topStudent = "";
+        double maxBonus = double.MinValue;
+
+        for (int i = 0; i < numberOfStudents; i++)
+        {
+            int studentAttendances = int.Parse(Console.ReadLine());
+            double bonus = (double)studentAttendances / numberOfLectures * (5 + additionalBonus);
+
+            if (bonus > maxBonus)
+            {
+                maxBonus = bonus;
+                topStudent = $"Max Bonus: {Math.Ceiling(maxBonus)}.";
+            }
+        }
+
+        Console.WriteLine(topStudent);
+        Console.WriteLine($"The student has attended {Math.Round(maxBonus / (5 + additionalBonus) * numberOfLectures)} lectures.");
+    }
+}
