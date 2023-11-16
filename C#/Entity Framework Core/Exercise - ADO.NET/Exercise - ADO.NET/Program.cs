ed540efa867ddb24accb0c1008820a95ab98c86d
Sema Yasharova <semayasharova@gmail.com>
2023-11-16 18:53:24 +0200

    New Files

---
+++ C#/Entity Framework Core/Exercise - ADO.NET/Exercise - ADO.NET/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace Exercise___ADO.NET
+{
+    internal class Program
+    {
+        //ServerName: DESKTOP-HRCKOHJ
+        //DataBase: MinionsDB
+
+        //1. Connection string
+          const string _connectionString = "Server=DESKTOP-HRCKOHJ;Database=MinionsDB;Integrated Security=True";
+
+
+        static void Main(string[] args)
+        {
+            //2. SQL connection
+            SqlConnection sqlConnection = new SqlConnection(_connectionString);
+            sqlConnection.Open();
+
+
+            //EXC: Villians Names:
+            //3. Create SQL command
+            using SqlCommand getVilliansCommand = new SqlCommand(SqlQueries.GetVillainsWithNumberOfMinions, sqlConnection);
+            using SqlDataReader sqlDataReader = getVilliansCommand.ExecuteReader();
+
+            //4. Data Reader
+            while (sqlDataReader.Read())
+            {
+                Console.WriteLine($"{sqlDataReader["Name"]} - {sqlDataReader["TotalMinions"]}");
+            }
+        }
+    }
+}
---
+++ C#/Entity Framework Core/Exercise - ADO.NET/Exercise - ADO.NET/SqlQueries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise___ADO.NET
+{
+    public static class SqlQueries
+    {
+        public const string GetVillainsWithNumberOfMinions = @"SELECT [Name], COUNT(*)[TotalMinions]
+                                                        FROM Villains v
+                                                        JOIN MinionsVillains mv ON mv.VillainId = v.Id
+                                                        GROUP BY [Name]
+                                                        HAVING COUNT(*) > 3
+                                                        ORDER BY TotalMinions";
+
+        public const string GetVilliansByID = @"SELECT Name FROM Villains WHERE Id = @Id";
+
+        public const string GetOrderedVilliansByUsingID = @"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) AS RowNum,
+                                                            m.Name, 
+                                                            m.Age
+                                                            FROM MinionsVillains AS mv
+                                                            JOIN Minions As m ON mv.MinionId = m.Id
+                                                            WHERE mv.VillainId = @Id
+                                                            ORDER BY m.Name";
+
+
+    }
+}
